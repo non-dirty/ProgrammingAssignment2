04c9e3828e3474ab1c703253bcf7584f215f307a
Matthew Smith <matthew@sustainablematters.org>
2014-08-19 21:31:35 -0400

    First pass of matrix inverse caching based on example

--- cachematrix.R
+++ cachematrix.R
@@ -5,11 +5,42 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  inverse <- NULL
+  
+  get <- function(){
+    x
+  }
+  
+  set <- function(aMatrix){
+    x <<- aMatrix
+    inverse <<- NULL
+  }
+  
+  getInverse <- function(){
+    inverse
+  }
+  
+  setInverse <- function(anInverse){
+    inverse <<- anInverse
+  }
+  
+  list(get=get, set=set, getInverse=getInverse, setInverse=setInverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+  inverse <- x$getInverse()
+  if(!is.null(inverse)){
+    message("using cached inverse")
+    return(inverse)
+  }
+  
+  data <- x$get()
+  inverse <- solve(data,...)
+  x$setInverse(inverse)
+  
+  return(inverse)
 }
